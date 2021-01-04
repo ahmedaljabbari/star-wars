233ad9d2cfe8f06f4016f94e9f1449a3e403548c
ahmedaljabbari <ahmedaljabbari@outlook.com>
2021-01-04 03:13:46 +0100

    Search box styling

--- src/components/API.js
+++ src/components/API.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../App.css";
 
 
 const API = () => {
@@ -38,11 +39,11 @@ const API = () => {
       return setServerData(filteredElements);
   };
 
-  const elements = serverData.map((result, index) => {
+  const elmentsList = serverData.map((result, index) => {
     return (
       <div key={result.name + index}>
         <p>{result.name}</p>
-        <span>fav</span>
+        <span>❤️</span>
         <p>Eye color: <span>{result.eye_color}</span></p>
         <p>Birth year: <span>{result.birth_year}</span></p>
       </div >
@@ -50,11 +51,29 @@ const API = () => {
   })
 
   return (
-    <div className="search-bar">
-      <input value={inputString} onChange={event => setInputString(event.target.value.toLowerCase())} type="text" placeholder="Enter valid srerch text" />
-      <button onClick={getWars}>Search</button>
-      <div>
-        {elements}
+    <div className="container">
+      <section className="search-box">
+        <div id="logo">
+          Star Wars
+        </div>
+        <div id="sub">
+          <input id="txt" value={inputString} onChange={event => setInputString(event.target.value.toLowerCase())} type="text" placeholder="Enter valid search text" />
+          <button id="btn" onClick={getWars}>Search</button>
+        </div>
+        <span id="add-section">
+          +
+        </span>
+      </section>
+
+      <div className="list-container">
+        <div>
+          <h3>Main list</h3>
+          {elmentsList}
+        </div>
+        <div>
+          <h3>Favorites list</h3>
+          
+        </div>
       </div>
     </div>
   )
